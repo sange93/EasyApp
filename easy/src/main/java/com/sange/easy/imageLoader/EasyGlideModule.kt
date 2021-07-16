3e77423a80e41140779e6dab36c7ee400f076793
sangshengqi <sangshengqi@zhuiji.com>
2021-07-16 16:23:40 +0800

    1、忽略glide 警告；

--- easy/src/main/java/com/sange/easy/imageLoader/EasyGlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.sange.easy.imageLoader
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.LibraryGlideModule
-
-@GlideModule
-class EasyGlideModule: LibraryGlideModule()
